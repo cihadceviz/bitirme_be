dbde16210d8e6b69310239a210e1012057a73299
scihadc <[cihad.ceviz@siemplus.com]>
2021-04-25 14:58:02 +0300

    -response değerler eklendi

--- app/Http/Controllers/activitiesController.php
+++ app/Http/Controllers/activitiesController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\activitiesDetailModel;
 use App\Models\activitiesModel;
 use App\Models\activitiesStatusModel;
+use http\Env\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class activitiesController extends Controller
 {
-    public function saveActivity(Request $request){
+    public function saveActivity(Request $request)
+    {
         $user_id = $request->input('user_id');
         $activities_name = $request->input('activity_name');
         $activity_start_date = $request->input('activity_start_date');
@@ -23,34 +25,48 @@ class activitiesController extends Controller
         $json_invited_user = json_encode($activity_invited_user);
 
 
+        try {
+            activitiesDetailModel::insert([
+                'activity_status' => $activity_status,
+                'activity_name' => $activities_name,
+                'activity_createdBy' => $user_id,
+                'activity_start_date' => $activity_start_date,
+                'activity_end_date' => $activity_end_date,
+                'activity_invited_user' => $json_invited_user,
+                'activity_view' => $activity_view,
+                'activity_description' => $activity_description
 
-        activitiesDetailModel::insert([
-            'activity_status'=>$activity_status,
-            'activity_name'=>$activities_name,
-            'activity_createdBy'=>$user_id,
-            'activity_start_date'=>$activity_start_date,
-            'activity_end_date'=>$activity_end_date,
-            'activity_invited_user'=>$json_invited_user,
-            'activity_view'=>$activity_view,
-            'activity_description'=>$activity_description
-
-        ]);
-        $res = activitiesDetailModel::all()->last();
-        $activity_id = ($res['activity_id']);
-
-
-        $elementCount  = count($activity_invited_user);
-        for ($i = 1; $i<$elementCount+1; $i++ ){
-            $str = "obj%d";
-            $lastStr = sprintf($str, $i);
-            $user_id = $activity_invited_user[$lastStr]['user'];
-
-            activitiesModel::insert([
-                'activity_id'=>$activity_id,
-                'user_id'=>$user_id
             ]);
 
+            $res = activitiesDetailModel::all()->last();
+            $activity_id = ($res['activity_id']);
+            $elementCount = count($activity_invited_user);
+
+            for ($i = 1; $i < $elementCount + 1; $i++) {
+                $str = "obj%d";
+                $lastStr = sprintf($str, $i);
+                $user_id = $activity_invited_user[$lastStr]['user'];
+
+                activitiesModel::insert([
+                    'activity_id' => $activity_id,
+                    'user_id' => $user_id
+                ]);
+
+            }
+            return response()->json([
+                'status'=>'200',
+                'res'=>'Activity Created',
+            ],200);
+
+        } catch (Throwable $e) {
+
+            report($e);
+            return response()->json([
+                'status' => '403',
+                'error' => $e
+            ], 403);
         }
+
 
     }
 }
